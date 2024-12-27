a121fe64b111c1fab4c0145081039a4da7ff2cd1
ManeeshaMaduranga <MadurangaBandara1006@gmail.com>
2024-12-27 20:46:39 +0530

    create movie model

---
+++ Movies.Application/Models/Movie.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Movies.Application.Models
+{
+    public class Movie
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int YearOfRelease { get; set; }
+        public List<string> Generes { get; set; }
+    }
+}
