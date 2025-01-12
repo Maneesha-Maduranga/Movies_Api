e1f62620ec15e6ac1fad6a0a738d5a76ab31753c
ManeeshaMaduranga <MadurangaBandara1006@gmail.com>
2025-01-12 11:34:59 +0530

    Implement db factory

---
+++ Movies.Application/Database/DbConnectionFactory.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+
+namespace Movies.Application.Database
+{
+    public interface IDbConnectionFactory
+    {
+        Task<IDbConnection> CreateConnectionAsync();
+    }
+
+    public class SqlConnectionFactory : IDbConnectionFactory
+    {
+        private readonly string _connectionString;
+
+        public SqlConnectionFactory(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public async Task<IDbConnection> CreateConnectionAsync()
+        {
+          var connection = new SqlConnection(_connectionString);
+          await connection.OpenAsync();
+          return connection;
+        }
+    }
+}
